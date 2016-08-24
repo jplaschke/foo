de1afad4655512fab88c593eff95f7b4f9759622
John Plaschke <plaschkej@gmail.com>
2016-08-23 22:39:35 -0400

    updates

--- lawapp/urls.py
+++ lawapp/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^$', auth_views.login),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^lawdb/', include('lawdb.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- lawdb/views.py
+++ lawdb/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def index(request):
     return HttpResponse("Hello, world")
